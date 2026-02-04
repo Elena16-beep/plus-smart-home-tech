6995178ae1b9954289864f692fdbeaa46b5e285e
Elena Nikolaeva <elenanikolaevaya@yandex.ru>
2026-02-04 15:27:27 +0500

    Проект Smart Home Technologies (plus-smart-home-tech). Analizer v.21 (спринт 20)

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/dal/model/Scenario.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/dal/model/Scenario.java
@@ -1,38 +1,38 @@
-package ru.yandex.practicum.dal.model;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@Table(name = "scenarios",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"hub_id", "name"})
-        })
-@Getter
-@Setter
-@Builder
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-public class Scenario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "hub_id", nullable = false)
-    private String hubId;
-
-    @Column(nullable = false)
-    private String name;
-}
+//package ru.yandex.practicum.dal.model;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import jakarta.persistence.UniqueConstraint;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Entity
+//@Table(name = "scenarios",
+//        uniqueConstraints = {
+//                @UniqueConstraint(columnNames = {"hub_id", "name"})
+//        })
+//@Getter
+//@Setter
+//@Builder
+//@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Scenario {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "hub_id", nullable = false)
+//    private String hubId;
+//
+//    @Column(nullable = false)
+//    private String name;
+//}
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/dal/repository/ScenarioActionRepository.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/dal/repository/ScenarioActionRepository.java
@@ -1,20 +1,20 @@
-package ru.yandex.practicum.dal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.dal.model.Scenario;
-import ru.yandex.practicum.dal.model.ScenarioAction;
-import ru.yandex.practicum.dal.model.ScenarioActionId;
-import java.util.List;
-
-@Repository
-public interface ScenarioActionRepository extends JpaRepository<ScenarioAction, ScenarioActionId> {
-
-    void deleteByScenario(Scenario scenario);
-
-    List<ScenarioAction> findByScenario(Scenario scenario);
-
-    List<ScenarioAction> findAllByScenarioIdIn(List<Long> scenarioIds);
-
-    void deleteAllByScenarioId(Long scenarioId);
-}
+//package ru.yandex.practicum.dal.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import ru.yandex.practicum.dal.model.Scenario;
+//import ru.yandex.practicum.dal.model.ScenarioAction;
+//import ru.yandex.practicum.dal.model.ScenarioActionId;
+//import java.util.List;
+//
+//@Repository
+//public interface ScenarioActionRepository extends JpaRepository<ScenarioAction, ScenarioActionId> {
+//
+//    void deleteByScenario(Scenario scenario);
+//
+//    List<ScenarioAction> findByScenario(Scenario scenario);
+//
+//    List<ScenarioAction> findAllByScenarioIdIn(List<Long> scenarioIds);
+//
+//    void deleteAllByScenarioId(Long scenarioId);
+//}
