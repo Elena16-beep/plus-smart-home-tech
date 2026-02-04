0d81b8dc9e57f6ff2bb4ff1416fabe9863040038
Elena Nikolaeva <elenanikolaevaya@yandex.ru>
2026-02-04 10:21:07 +0500

    Проект Smart Home Technologies (plus-smart-home-tech). Analizer v.7 Aggregator (спринт 20)

--- telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/deserializer/BaseAvroDeserializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/deserializer/BaseAvroDeserializer.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.kafka.deserializer;
 
 import org.apache.avro.Schema;
 import org.apache.avro.io.BinaryDecoder;
-import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificRecordBase;
@@ -12,7 +11,7 @@ import java.io.IOException;
 
 public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
     private final DecoderFactory decoderFactory;
-    private final DatumReader<T> reader;
+    private final Schema schema;
 
     public BaseAvroDeserializer(Schema schema) {
         this(DecoderFactory.get(), schema);
@@ -20,13 +19,15 @@ public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deser
 
     public BaseAvroDeserializer(DecoderFactory decoderFactory, Schema schema) {
         this.decoderFactory = decoderFactory;
-        this.reader = new SpecificDatumReader<T>(schema);
+        this.schema = schema;
     }
 
     @Override
     public T deserialize(String topic, byte[] data) {
         try {
             if (data != null) {
+                var reader = new SpecificDatumReader<T>(schema);
+
                 BinaryDecoder decoder = decoderFactory.binaryDecoder(data, null);
 
                 return reader.read(null, decoder);
